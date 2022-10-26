a32fd22604086f32b6f7b39c7b63e69eebb4aae8
nyckolas <nyckolas.work@gmail.com>
2022-10-26 16:51:27 +0300

    Add api urls route

--- todo/api/urls.py
+++ todo/api/urls.py
@@ -1,10 +1,13 @@
-from django.urls import include, path
-from rest_framework import routers
+from django.urls import path
 from api import views
 
-from django.contrib import admin
 
 urlpatterns = [
     path('login/', views.user_login),
-    path('create-user/', views.CreateUser.as_view())
+    path('create-user/', views.RegisterUserView.as_view()),
+    path('v1/task/', views.TaskListView.as_view()),
+    path('v1/task/<int:pk>', views.TaskDetailView.as_view(),
+         name='task_update'),
+    path('v1/create-images/', views.CreateImagesView.as_view(),
+         name='img_create'),
 ]
